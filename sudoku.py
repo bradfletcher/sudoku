a2f245fa35369ef630ab54a4c0fe8fc4d7b69ec3
Brad Fletcher <brad@udel.edu>
2020-06-24 23:41:28 -0400

    adding row and cols of text boxes

--- sudoku.py
+++ sudoku.py
@@ -6,18 +6,21 @@ canvas1 = tk.Canvas(root, width = 800, height = 800 )
 canvas1.pack()
 
 entry1 = tk.Entry (root) 
-canvas1.create_window(200, 140, window=entry1)
+canvas1.create_window(650, 650, window=entry1) 
 
-entry2 = tk.Entry (root) 
-canvas1.create_window(200, 160, window=entry2) 
+for x in range(9):  
+    for y in range(9):  
+        valueEntry = tk.Entry (root, width = 1 ) 
+        canvas1.create_window(100 + 50*x, 100 + 50*y , window=valueEntry) 
 
 def getSquareRoot ():  
-    x1 = entry1.get()
+    x1 = entry1.get() 
     
-    label1 = tk.Label(root, text= float(x1)**0.5)
-    canvas1.create_window(200, 230, window=label1)
+    label1 = tk.Label(root, text= float(x1)**0.5) 
+    canvas1.create_window(650, 680, window=label1) 
     
-button1 = tk.Button(text='Get the Square Root', command=getSquareRoot)
-canvas1.create_window(200, 180, window=button1)
-
-root.mainloop()
+button1 = tk.Button(text='Get the Square Root', command=getSquareRoot) 
+canvas1.create_window(650, 720, window=button1) 
+ 
+root.mainloop() 
+ 
